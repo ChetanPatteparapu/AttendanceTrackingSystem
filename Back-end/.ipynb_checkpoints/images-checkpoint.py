a376db65151560fe33e0a3751fe9efe633c7a443
Chetan Patteparapu <chetan.patteparapu@gmail.com>
2021-03-06 13:33:44 +0000

    initial commit

---
+++ Back-end/.ipynb_checkpoints/images-checkpoint.py
@@ -0,0 +1,30 @@
+import os
+import cv2
+import face_recognition
+
+# IMPORT IMAGES
+path = 'student_images'
+images = []
+student_names = []
+myList = os.listdir(path)
+
+for rawImg in myList:
+    current_img = cv2.imread(f'{path}/{rawImg}')
+    images.append(current_img)
+    student_names.append(os.path.splitext(rawImg)[0])
+    
+# GET ENCODINGS OF EXISTING IMAGES IN THE LIST    
+def getEncodingOfExistingImages():
+    global images
+    
+    listOfEncodings = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_encoding = face_recognition.face_encodings(img)[0]
+        listOfEncodings.append(img_encoding)
+    return listOfEncodings
+
+
+def getStudentNames():
+    global student_names
+    return student_names
